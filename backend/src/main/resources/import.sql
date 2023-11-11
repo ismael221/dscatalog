f2fcdd69e6ade0e801deb7af301bf712b3ec0b91
Ismael Nunes Campos <ismaeldenunes@gmail.com>
2023-11-10 22:23:52 -0300

    database seeding

---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,7 @@
+INSERT INTO tb_category(name) VALUES('Books');
+INSERT INTO tb_category(name) VALUES('Eletrocnics');
+INSERT INTO tb_category(name) VALUES('Computers');
+INSERT INTO tb_category(name) VALUES('Manga');
+INSERT INTO tb_category(name) VALUES('TV');
+INSERT INTO tb_category(name) VALUES('Cups');
+
